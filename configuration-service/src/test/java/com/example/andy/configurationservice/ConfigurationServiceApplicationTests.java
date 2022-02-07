b7236e2035e815306e1eb50f1cae157725fc9377
andrii.yanko <andrii.yanko@globallogic.com>
2022-02-07 18:45:02 +0200

    Initial commit. Realize configuration of microservices and configuration-service pom.xml

---
+++ configuration-service/src/test/java/com/example/andy/configurationservice/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.andy.configurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
